278008c87d30f879835fbad618e2a90646d215cd
karlaemartins <karlaemartinss@gmail.com>
2026-07-02 10:15:52 -0300

    feat: add penguin spawn cycle

--- Project14-WhackAPenguin/GameScene.swift
+++ Project14-WhackAPenguin/GameScene.swift
@@ -7,30 +7,34 @@
 import SpriteKit
 
 final class GameScene: SKScene {
-    
     private var gameScore: SKLabelNode!
     private var slots = [WhackSlot]()
+    private var popupTime: TimeInterval = 0.85
     private var score = 0 {
         didSet {
             gameScore.text = "Score: \(score)"
         }
     }
-    
+
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "whackBackground")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
-        
+
         gameScore = SKLabelNode(fontNamed: "Chalkduster")
         gameScore.text = "Score: 0"
         gameScore.position = CGPoint(x: 8, y: 8)
         gameScore.horizontalAlignmentMode = .left
         gameScore.fontSize = 48
         addChild(gameScore)
-        
+
         createSlots()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
+        }
     }
 }
 
@@ -39,25 +43,45 @@ private extension GameScene {
         for index in 0..<5 {
             createSlot(at: CGPoint(x: 100 + (index * 170), y: 410))
         }
-        
+
         for index in 0..<4 {
             createSlot(at: CGPoint(x: 180 + (index * 170), y: 320))
         }
-        
+
         for index in 0..<5 {
             createSlot(at: CGPoint(x: 100 + (index * 170), y: 230))
         }
-        
+
         for index in 0..<4 {
             createSlot(at: CGPoint(x: 180 + (index * 170), y: 140))
         }
-        
-        func createSlot(at position: CGPoint) {
-            let slot = WhackSlot()
-            slot.configure(at: position)
-            
-            addChild(slot)
-            slots.append(slot)
+    }
+
+    func createSlot(at position: CGPoint) {
+        let slot = WhackSlot()
+        slot.configure(at: position)
+
+        addChild(slot)
+        slots.append(slot)
+    }
+
+    func createEnemy() {
+        popupTime *= 0.991
+
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+
+        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime) }
+
+        let minDelay = popupTime / 2
+        let maxDelay = popupTime * 2
+        let delay = Double.random(in: minDelay...maxDelay)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.createEnemy()
         }
     }
 }
--- Project14-WhackAPenguin/Scenes/WhackSlot.swift
+++ Project14-WhackAPenguin/Scenes/WhackSlot.swift
@@ -9,6 +9,8 @@ import SpriteKit
 
 final class WhackSlot: SKNode {
     private(set) var charNode: SKSpriteNode!
+    private(set) var isVisible = false
+    private(set) var isHit = false
 
     func configure(at position: CGPoint) {
         self.position = position
@@ -27,5 +29,35 @@ final class WhackSlot: SKNode {
 
         cropNode.addChild(charNode)
         addChild(cropNode)
+    }
+    
+    func show(hideTime: TimeInterval) {
+        guard !isVisible else { return }
+        
+        charNode.xScale = 1
+        charNode.yScale = 1
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        guard isVisible else { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
     }
 }
