51b56372660c1ab4d644ee370a4f4b338cd93356
karlaemartins <karlaemartinss@gmail.com>
2026-07-02 09:46:46 -0300

    Feat: add masked penguin slots

--- Project14-WhackAPenguin/Scenes/WhackSlot.swift
+++ Project14-WhackAPenguin/Scenes/WhackSlot.swift
@@ -8,10 +8,24 @@
 import SpriteKit
 
 final class WhackSlot: SKNode {
+    private(set) var charNode: SKSpriteNode!
+
     func configure(at position: CGPoint) {
         self.position = position
 
         let holeNode = SKSpriteNode(imageNamed: "whackHole")
         addChild(holeNode)
+
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+
+        cropNode.addChild(charNode)
+        addChild(cropNode)
     }
 }
